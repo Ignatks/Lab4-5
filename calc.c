3ce32d256c7a2bd800bab71d6cecce40d2f31d11
Ignatks <19932008iI>
2014-06-03 13:02:29 +0400

    added calculate function
    added calculate function for sysfs

--- calc.c
+++ calc.c
@@ -1,34 +1,62 @@
+#define SYSFS
+
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
+
+#ifdef SYSFS
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#else
 #include <linux/proc_fs.h>
+#endif
 
-#define WRITE_SIZE 100
-
+// names
 #define ARG1 "arg1"
 #define ARG2 "arg2"
 #define OPERATION "operation"
 #define RESULT "result"
 
 #define PARENT_DIR "calc"
-
-struct proc_dir_entry *calc_dir;
-struct proc_dir_entry *arg1;
-struct proc_dir_entry *arg2;
-struct proc_dir_entry *operation;
-struct proc_dir_entry *result;
-
+#define WRITE_SIZE 100
 
 static char arg1_input[WRITE_SIZE];
 static char arg2_input[WRITE_SIZE];
 static char operation_input[WRITE_SIZE];
+
+
+long calculate(void) {
+	long a1 = 0;
+	long a2 = 0;
+	long res = 0;
+
+	if (arg1_input[strlen(arg1_input) - 2] == '\n') {
+		arg1_input[strlen(arg1_input) - 2] = (char)0;
+	}
+
+	kstrtol(arg1_input, 10, &a1);
+	kstrtol(arg2_input, 10, &a2);
+
+	if (operation_input[0] == '+') {
+		res = a1 + a2;
+	} else if (operation_input[0] == '-') {
+		res = a1 - a2;
+	} else if (operation_input[0] == '.') {
+		res = a1 * a2;
+	} else if (operation_input[0] == '/') {
+		res = a1 / a2;
+	}
+	return res;
+}
 
 /*
  * arg1 write handler
  */
 int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
 {
-	if (count > WRITE_SIZE) {
+	if(count > WRITE_SIZE) {
     	count = WRITE_SIZE;
 	}
 
@@ -42,7 +70,7 @@ int write_arg1(struct file *file, const char *buf, unsigned long count, void *da
  */
 int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
 {
-	if (count > WRITE_SIZE) {
+	if(count > WRITE_SIZE) {
     	count = WRITE_SIZE;
 	}
 
@@ -56,7 +84,7 @@ int write_arg2(struct file *file, const char *buf, unsigned long count, void *da
  */
 int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
 {
-	if (count > WRITE_SIZE) {
+	if(count > WRITE_SIZE) {
     	count = WRITE_SIZE;
 	}
 
@@ -99,41 +127,41 @@ int init_module()
 {
 	// parent dir
 	calc_dir = proc_mkdir(PARENT_DIR, NULL);
-	if (!calc_dir) {
+	if(!calc_dir) {
 		printk(KERN_INFO "Error creating proc entry");
 		return -ENOMEM;
 	}
 
 	// arg1
 	arg1 = create_proc_entry(ARG1, 0666, calc_dir);
-	if (!arg1) {
+	if(!arg1) {
     	printk(KERN_INFO "Error creating proc entry");
     	return -ENOMEM;
-	}
+    }
 	arg1->write_proc = write_arg1;
 
 	// arg2
 	arg2 = create_proc_entry(ARG2, 0666, calc_dir);
-	if (!arg2) {
+	if(!arg2) {
     	printk(KERN_INFO "Error creating proc entry");
     	return -ENOMEM;
-	}
+    }
 	arg2->write_proc = write_arg2;
 
 	// operation
 	operation = create_proc_entry(OPERATION, 0666, calc_dir);
-	if (!operation) {
+	if(!operation) {
     	printk(KERN_INFO "Error creating proc entry");
     	return -ENOMEM;
-    	}
+    }
 	operation->write_proc = write_operation;
 
 	// result
 	result = create_proc_entry(RESULT, 0666, calc_dir);
-	if (!result) {
+	if(!result) {
     	printk(KERN_INFO "Error creating proc entry");
     	return -ENOMEM;
-    	}
+    }
 	result->read_proc = read_result;
 
 	printk(KERN_INFO "/proc/%s created\n", PARENT_DIR);
